02a17d064b472d8b66f0a175e32597d1cd269402
Szymon Winiarski <93779@student.ue.poznan.pl>
2024-04-28 17:11:15 +0200

    ver. 1.0 licz_sume

---
+++ licz_sume.py
@@ -0,0 +1,32 @@
+
+
+def licz_sume(dane_wejsciowe):
+    # Przeliczniki
+    przelicznik_sykli_na_geleony = 17
+    przelicznik_knutow_na_sykli = 21
+
+    # Podawanie liczby
+    ilosc_geleonow = int(input("Podaj ilość geleonów: "))
+    ilosc_sykli = int(input("Podaj ilość sykli: "))
+    ilosc_knutow = int(input("Podaj ilość knutów: "))
+
+    suma_knutow = ilosc_knutow + ilosc_sykli * przelicznik_knutow_na_sykli + ilosc_geleonow * przelicznik_sykli_na_geleony * przelicznik_knutow_na_sykli
+
+    # Przeliczenie na monety o największym nominale
+    ilosc_geleonow_po_przeliczeniu = suma_knutow // (przelicznik_sykli_na_geleony * przelicznik_knutow_na_sykli)
+    suma_knutow %= przelicznik_sykli_na_geleony * przelicznik_knutow_na_sykli
+    ilosc_sykli_po_przeliczeniu = suma_knutow // przelicznik_knutow_na_sykli
+    suma_knutow %= przelicznik_knutow_na_sykli
+
+    # Tworzenie wynikowego słownika
+    wynik = {
+        'geleon': ilosc_geleonow_po_przeliczeniu,
+        'sykl': ilosc_sykli_po_przeliczeniu,
+        'knut': suma_knutow
+    }
+
+    return wynik
+
+# Przykładowe użycie funkcji
+dane = {'geleon': 0, 'sykl': 0, 'knut': 0}
+print(licz_sume(dane))
