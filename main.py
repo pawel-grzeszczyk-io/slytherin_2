ad8c63bd7d4fd277bc90d1cfecd3da01a76206d1
Andrzej Bednarz <andrzej.bednarz02@gmail.com>
2024-05-19 21:03:44 +0200

    dodaję zadanie 8

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
 import time
 import random
 import csv
+from datetime import datetime
+import pandas as pd
 
 def wybierz_sowe_zwroc_koszt(potwierdzenie_odbioru, odleglosc, typ, specjalna):
     koszt = 0 # wpisz koszt
@@ -142,4 +144,37 @@ def nadaj_sowe(adresat, tresc_wiadomosci, potwierdzenie_odbioru, odleglosc, typ,
         writer.writerow([adresat, tresc_wiadomosci, koszt_str, potwierdzenie_str])
 
 nadaj_sowe("Zuza", "Testujemy czy system nam dziala", True, 'lokalna', 'list', 'wyjec')
+
+
+
+def poczta_wyslij_sowy(sciezka_do_pliku):
+    column_names = ['adresat', 'treść wiadomości', 'koszt przesyłki', 'potwierdzenie odbioru']
+    data = pd.read_csv(sciezka_do_pliku, encoding='latin1', header=None, names=column_names)
+    wyniki = []
+    for index, wiersz in data.iterrows():
+        adresat = wiersz['adresat']
+        tresc_listu = wiersz['treść wiadomości']
+        koszt_przesylki = wiersz['koszt przesyłki']
+        potwierdzenie_odbioru = wiersz['potwierdzenie odbioru']
+
+        wysylka_sowy = wyslij_sowe(adresat, tresc_listu)
+        if wysylka_sowy or potwierdzenie_odbioru == "TAK":
+            rzeczywisty_koszt = 0
+        else:
+            rzeczywisty_koszt = koszt_przesylki
+
+        wyniki.append({
+            'adresat': adresat,
+            'tresc wiadomosci': tresc_listu,
+            'koszt przesylki': koszt_przesylki,
+            'potwierdzenie odbioru': potwierdzenie_odbioru,
+            'rzeczywisty koszt': rzeczywisty_koszt
+        })
+
+    wyniki_df = pd.DataFrame(wyniki)
+    wyniki_df.to_csv('output_sowy_z_poczty_dzien_miesiac_rok.csv')
+
+    
+poczta_wyslij_sowy('poczta_nadania_lista.csv')
+
 
