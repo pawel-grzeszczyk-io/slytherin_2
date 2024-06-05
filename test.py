86fccdf4ea07b8ada107b9f54cf2623d454c0741
andrzejbednarz1 <164729240+andrzejbednarz1@users.noreply.github.com>
2024-06-05 21:38:16 +0200

    Update test.py

--- test.py
+++ test.py
@@ -1,10 +1,7 @@
-from main import waluta_dict_na_str 
-
+from main import waluta_dict_na_str
 
 def test_waluta_dict_na_str():
-    x = waluta_dict_na_str{ "galeon" : 0, "sykl" : 0, "knut" : 13}
-
-    
-    assert x == ("13 knut")
+    x = waluta_dict_na_str({"galeon": 0, "sykl": 0, "knut": 13})
+    assert x == "13 knut"
 
 
